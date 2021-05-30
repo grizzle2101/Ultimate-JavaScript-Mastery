a5a25e95d1c5717aaf029ce11e31cd636c436e65
Conor McGrath <conormcgrath123@gmail.com>
2021-05-30 14:01:39 +0100

    part 1 - operators - arithmetic operators

---
+++ Part 1 - Fundamentals/2 - Operators/starting-point/index.js
@@ -0,0 +1,20 @@
+
+//Lecture 2 - Arithmetic Operators:
+let x = 10;
+let y = 3;
+
+console.log(x + y); //addition operator
+console.log(x - y); //subtraction operator
+console.log(x + y); //multiplication operator
+console.log(x / y); //division operator
+console.log(x % y); //remainer of division operator
+console.log(x ** y); //NEW - exponentiation operator (to the power of)
+
+//Increnet operator
+//console.log(x);
+//console.log(++x);
+//console.log(x++);
+
+//Decrement operator
+console.log(x);
+console.log(--x);
