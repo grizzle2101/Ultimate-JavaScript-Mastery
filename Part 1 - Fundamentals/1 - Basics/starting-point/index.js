6aff702dd058640a2c839c14c6e042cf804fc518
Conor McGrath <conormcgrath123@gmail.com>
2021-05-30 10:55:09 +0100

    part 1 - fundamentals - variables

--- Part 1 - Fundamentals/1 - Basics/starting-point/index.js
+++ Part 1 - Fundamentals/1 - Basics/starting-point/index.js
@@ -1,2 +1,18 @@
-// this is my first JavaScript code!
-console.log('Hello World!')
+//var name - deprecated sinced ES6
+let name = 'Mosh';
+console.log(name);
+
+
+/* Rules:
+-Cannot be a reserved keyword
+-Should be meaningful
+-Cannot start with a number (1number)
+-Cannot contain a space of hyphen (-)
+- Are case sensitive
+*/
+
+// Multiple Variables
+let firstName = 'Mosh', secondName = 'Hammedani';
+console.log(firstName ,secondName);
+
+//note - this is not a best practice, should break up declarations onto their own line.
