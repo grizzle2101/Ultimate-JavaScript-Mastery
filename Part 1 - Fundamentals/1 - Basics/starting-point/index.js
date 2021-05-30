1b34b6f1b47f25db262f839c0666b4da912b64e7
Conor McGrath <conormcgrath123@gmail.com>
2021-05-30 10:58:30 +0100

    part 1 - fundamentals - constants

--- Part 1 - Fundamentals/1 - Basics/starting-point/index.js
+++ Part 1 - Fundamentals/1 - Basics/starting-point/index.js
@@ -1,3 +1,4 @@
+//Lecutre 1 - Variables:
 //var name - deprecated sinced ES6
 let name = 'Mosh';
 console.log(name);
@@ -16,3 +17,11 @@ let firstName = 'Mosh', secondName = 'Hammedani';
 console.log(firstName ,secondName);
 
 //note - this is not a best practice, should break up declarations onto their own line.
+
+
+//Lecture 2 - Constants:
+const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+//note - if you know a value should NOT change, use CONST over LET.
