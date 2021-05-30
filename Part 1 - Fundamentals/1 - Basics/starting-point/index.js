ef006bf37f5c337c9c8b2dcb48232cadc4b6bfdc
Conor McGrath <conormcgrath123@gmail.com>
2021-05-30 11:10:56 +0100

    part 1 - fundamentals - dynamic typing

--- Part 1 - Fundamentals/1 - Basics/starting-point/index.js
+++ Part 1 - Fundamentals/1 - Basics/starting-point/index.js
@@ -1,7 +1,7 @@
 //Lecutre 1 - Variables:
 //var name - deprecated sinced ES6
-let name = 'Mosh';
-console.log(name);
+//let name = 'Mosh';
+//console.log(name);
 
 
 /* Rules:
@@ -13,15 +13,15 @@ console.log(name);
 */
 
 // Multiple Variables
-let firstName = 'Mosh', secondName = 'Hammedani';
-console.log(firstName ,secondName);
+//let firstName = 'Mosh', secondName = 'Hammedani';
+//console.log(firstName ,secondName);
 
 //note - this is not a best practice, should break up declarations onto their own line.
 
 
 //Lecture 2 - Constants:
 const interestRate = 0.3;
-interestRate = 1;
+//interestRate = 1;
 console.log(interestRate);
 
 //note - if you know a value should NOT change, use CONST over LET.
@@ -36,3 +36,14 @@ let age = 30; // number literal
 let isApproved = true // boolean literal
 let firstName; // undefined
 let lastName = null; //null
+
+
+//Lecture 4 - Dynamic Typing:
+//static languages v dynamic languaes
+// With dynamic languages, values can be changed @ runtime, and not with Static Languages.
+
+// demo - dynamic variables - reassign string literal
+let testName = 'Test'
+console.log(typeof(testName));
+testName = 123;
+console.log(typeof(testName));
