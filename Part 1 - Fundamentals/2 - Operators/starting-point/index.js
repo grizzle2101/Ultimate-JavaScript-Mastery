3344808302afb6302b5f78b8911a9d1dabd2db1e
Conor McGrath <conormcgrath123@gmail.com>
2021-05-30 14:16:11 +0100

    part 1 - operators - equality operators

--- Part 1 - Fundamentals/2 - Operators/starting-point/index.js
+++ Part 1 - Fundamentals/2 - Operators/starting-point/index.js
@@ -1,46 +1,59 @@
 
-//Lecture 2 - Arithmetic Operators:
-let x = 10;
-let y = 3;
+// //Lecture 2 - Arithmetic Operators:
+// let x = 10;
+// let y = 3;
 
-console.log(x + y); //addition operator
-console.log(x - y); //subtraction operator
-console.log(x + y); //multiplication operator
-console.log(x / y); //division operator
-console.log(x % y); //remainer of division operator
-console.log(x ** y); //NEW - exponentiation operator (to the power of)
+// console.log(x + y); //addition operator
+// console.log(x - y); //subtraction operator
+// console.log(x + y); //multiplication operator
+// console.log(x / y); //division operator
+// console.log(x % y); //remainer of division operator
+// console.log(x ** y); //NEW - exponentiation operator (to the power of)
 
-//Increnet operator
-//console.log(x);
-//console.log(++x);
-//console.log(x++);
+// //Increnet operator
+// //console.log(x);
+// //console.log(++x);
+// //console.log(x++);
 
-//Decrement operator
-console.log(x);
-console.log(--x);
-
-
-//Lecture 3 - Assignment Operators
-let x = 10; //basic assignment operator
-x++; // x = x + 1, this is also an assignment operator.
-
-x += 5; //addition assignment operator. Shorthand for x = x + 5;
-x *= 3; //multiplication assignment operator
-
-//every arthmetic operator, also has an assignment operator version.
+// //Decrement operator
+// console.log(x);
+// console.log(--x);
 
 
+// //Lecture 3 - Assignment Operators
+// let x = 10; //basic assignment operator
+// x++; // x = x + 1, this is also an assignment operator.
 
-//Lecture 4 - Comparison Operator:
-let x = 1;
+// x += 5; //addition assignment operator. Shorthand for x = x + 5;
+// x *= 3; //multiplication assignment operator
 
-//relational operators
-console.log(x > 0); //result of comparison is true.
-console.log(x >= 0);
-console.log(x < 0); //false
-console.log(x <= 0);
+// //every arthmetic operator, also has an assignment operator version.
 
 
-//equality operator
-console.log(x === 1);
-console.log(x !== 1);
+
+// //Lecture 4 - Comparison Operator:
+// let x = 1;
+
+// //relational operators
+// console.log(x > 0); //result of comparison is true.
+// console.log(x >= 0);
+// console.log(x < 0); //false
+// console.log(x <= 0);
+
+
+// //equality operator
+// console.log(x === 1);
+// console.log(x !== 1);
+
+
+//Lecture 5 - Equality operators:
+
+//strict equality(ensures same type & value)
+let x = 1
+console.log(1 === 1);
+console.log(1 === '1');
+
+//loose equality
+console.log(1 == '1');
+
+console.log(true == '1');
