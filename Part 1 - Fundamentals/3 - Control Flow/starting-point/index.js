e6f02112e79782aa0325f4625f0377958ca160c7
Conor McGrath <conormcgrath123@gmail.com>
2021-05-31 14:18:59 +0100

    part 1 - control flow - if else

---
+++ Part 1 - Fundamentals/3 - Control Flow/starting-point/index.js
@@ -0,0 +1,13 @@
+//Control Flow
+//Lecture 1 - IF ELSE
+//Conditional Statements:
+//IF/ELSE & Case/Switch
+
+let hour = 12;
+
+if (hour >= 6 && hour < 12) 
+    console.log("Good Morning!");
+else if (hour >= 12 && hour <= 18) 
+    console.log("Good Afternoon!");
+else 
+    console.log("Good Evening");
