2c4e68d202eb857ee9b927c6982887bcb8245408
Conor McGrath <conormcgrath123@gmail.com>
2021-05-31 13:49:39 +0100

    part 1 - operators - bitwise operators

--- Part 1 - Fundamentals/2 - Operators/starting-point/index.js
+++ Part 1 - Fundamentals/2 - Operators/starting-point/index.js
@@ -1,4 +1,3 @@
-
 // //Lecture 2 - Arithmetic Operators:
 // let x = 10;
 // let y = 3;
@@ -19,7 +18,6 @@
 // console.log(x);
 // console.log(--x);
 
-
 // //Lecture 3 - Assignment Operators
 // let x = 10; //basic assignment operator
 // x++; // x = x + 1, this is also an assignment operator.
@@ -28,8 +26,6 @@
 // x *= 3; //multiplication assignment operator
 
 // //every arthmetic operator, also has an assignment operator version.
-
-
 
 // //Lecture 4 - Comparison Operator:
 // let x = 1;
@@ -40,34 +36,29 @@
 // console.log(x < 0); //false
 // console.log(x <= 0);
 
-
 // //equality operator
 // console.log(x === 1);
 // console.log(x !== 1);
 
-
 //Lecture 5 - Equality operators:
 
 //strict equality(ensures same type & value)
-let x = 1
+let x = 1;
 console.log(1 === 1);
-console.log(1 === '1');
+console.log(1 === "1");
 
 //loose equality
-console.log(1 == '1');
+console.log(1 == "1");
 
-console.log(true == '1');
-
-
+console.log(true == "1");
 
 //Lecture 6 - Ternary Opertor:
 //If a customer has over 100 points, we want to classify him as gold, else hes silver.
 //We can simplify all this logic into a single statement, using the ternary operator.
 let points = 90;
-let type = points > 100? 'Gold': 'Silver';
+let type = points > 100 ? "Gold" : "Silver";
 
 console.log(type);
-
 
 //Lecture 7 - Logical Operators:
 //3 Types of logical operators are AND, OR, NOT.
@@ -91,28 +82,24 @@ console.log(highIncome || goodCredit); //true
 goodCredit = false;
 console.log(highIncome || goodCredit); //also true (only 1 operand has to be true)
 
-
 // NOT operator (!)
-// Returns 
+// Returns
 highIncome = false;
-goodCredit = false
-eligibleForLoan = highIncome && goodCredit; 
-let applicationRefused  = !eligibleForLoan; //NOT operator
+goodCredit = false;
+eligibleForLoan = highIncome && goodCredit;
+let applicationRefused = !eligibleForLoan; //NOT operator
 
-console.log('Eligible:', eligibleForLoan);
-console.log('Refused:', applicationRefused);
-
-
+console.log("Eligible:", eligibleForLoan);
+console.log("Refused:", applicationRefused);
 
 //Lecture 8 - Logical Operaors with Non-booleans:
 //In JavaScript we have the ability to use logical operators on Non-boolean values.
 
 console.log(false || true); //true
-console.log(false || 'Mosh'); //returns Mosh
+console.log(false || "Mosh"); //returns Mosh
 console.log(false || 1); //returns 1
 //Logical operators do NOT always return true/false
 //But instead tries to intrept Falsey or Truthy
-
 
 //Falsey
 //undefined
@@ -124,16 +111,49 @@ console.log(false || 1); //returns 1
 
 //Anything that is not falsy IS truthy
 
-console.log(false || 'Mosh') //Mosh != Falsey, therefor this operation is truthy.
+console.log(false || "Mosh"); //Mosh != Falsey, therefor this operation is truthy.
 console.log(false || 1 || 2); // As soon as we find truthy value, return it.
 
 //pick a colour, or defaults to normal.
 let userColor = undefined;
-let defaultColor = 'blue';
+let defaultColor = "blue";
 let currentColor = userColor || defaultColor;
 
 console.log(currentColor);
 
-userColor = 'red';
+userColor = "red";
 currentColor = userColor || defaultColor;
 console.log(currentColor);
+
+//Lecture 9 - Bitwise Operators:
+
+//1 = 00000001
+//2 = 00000010
+//R = 00000010
+console.log(1 | 2); //bitwise OR (|| is logical OR)
+
+//1 = 00000001
+//2 = 00000010
+//R = 00000000
+console.log(1 & 2); //bitwise AND
+
+//real world example - User permission sytem. (read, write & execute)
+//We can use bytes to represent the user permission.
+//00000100 - read only
+//00000110 - read & write
+//00000111 - read, write & execute
+
+const readPermisson = 4; //decimal value for 100
+const writePermission = 2;
+const executePermission = 1;
+let myPermission = 0;
+
+myPermission = myPermission | readPermisson | writePermission;
+console.log("My Permission - ", myPermission);
+
+let message = myPermission & readPermisson ? "Yes" : "No";
+console.log("Message - ", message);
+
+//Take Home:
+//with bitwise OR, we can add permissions.
+//with bitwide AND, we can check to see permissions.
