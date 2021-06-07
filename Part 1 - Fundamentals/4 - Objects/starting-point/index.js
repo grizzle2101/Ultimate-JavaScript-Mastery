d99130e072fa874b9a377a71e614b622b92eb3c5
Conor McGrath <conormcgrath123@gmail.com>
2021-06-07 14:01:12 +0100

    part 1 - objects - factory functions

--- Part 1 - Fundamentals/4 - Objects/starting-point/index.js
+++ Part 1 - Fundamentals/4 - Objects/starting-point/index.js
@@ -27,3 +27,53 @@ circle.draw();
 //OOP - Object Oriented Programming.
 //function exists by itself, a function in a class is a method.
 //in the next section, we're going to look at different ways to create objects.
+
+//Lecture 2 - Factory Functions:
+//In the previous section we used the object literal notation to create an object.
+//The problem is to create other objects we're duplicating so much code!
+const circle1 = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+  isVisible: false,
+  draw: function () {
+    console.log("draw");
+  },
+};
+
+const circle2 = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+  isVisible: false,
+  draw: function () {
+    console.log("draw");
+  },
+};
+
+//How do we encapsulate methods so it can be shared with the other classes?
+//Answer - Using a Factory Function
+function createCircle(radius) {
+  return {
+    radius,
+    draw() {
+      console.log("draw");
+    },
+  };
+}
+
+//note - if our key and value are the same like radius: radius, we can remove the assignment
+//as JavaScript smart enough to map the input to the key.
+
+//Using these Objects
+const c1 = createCircle(1);
+console.log(c1);
+c1.draw();
+
+const c2 = createCircle(2);
+console.log(c2);
+c2.draw();
