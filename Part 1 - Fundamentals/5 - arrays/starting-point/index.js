6ed625b00e22a93f1f371b132908048871ef4fca
Conor McGrath <conormcgrath123@gmail.com>
2021-06-10 00:25:02 +0100

    part 1 - arrays - finding elements (primitivies)

--- Part 1 - Fundamentals/5 - arrays/starting-point/index.js
+++ Part 1 - Fundamentals/5 - arrays/starting-point/index.js
@@ -6,6 +6,23 @@ const numbers = [3, 4];
 
 numbers.push(5, 6); // add to end
 numbers.unshift(1, 2); //shifts items and adds into the start
-numbers.splice(2, 0, ["a", "b"]); //splice @ position 2, delete 0, insert array
+numbers.splice(2, 0, "a"); //splice @ position 2, delete 0, insert array
 
 console.log("numbers - ", numbers);
+
+//Lecture 3 - Finding Elements:
+//Findings depends if we're using primitive or reference types in the array...
+
+//find primitives
+console.log(numbers.indexOf("a")); //returns location of item A. -1 = not found.
+
+//find last index of a given item
+numbers.push("a");
+console.log(numbers.lastIndexOf("a")); //find A at end of array.
+
+//how to check if items exists:
+console.log(numbers.indexOf("a") !== -1); //overly verbose
+console.log(numbers.includes("a")); //more clean way
+
+//optional parameter - fromIndex
+console.log("end - ", numbers.indexOf("a", 3)); //We can skip beyond first occurence of an item.
