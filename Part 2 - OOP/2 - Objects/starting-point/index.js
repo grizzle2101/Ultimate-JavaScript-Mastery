21c03d0c11bb7758e270d9ea555d6397a5da8b31
Conor McGrath <conormcgrath123@gmail.com>
2021-06-19 22:17:42 +0100

    part 2 - objects - constructor property

--- Part 2 - OOP/2 - Objects/starting-point/index.js
+++ Part 2 - OOP/2 - Objects/starting-point/index.js
@@ -31,14 +31,14 @@ const testCircle = createCircle(2);
 testCircle.draw();
 
 //Lecture 4 - Constructor:
-function Cirlce(radius) {
+function Circle(radius) {
   (this.radius = radius),
     (this.draw = function () {
       console.log("constructor draw circle...");
     });
 }
 
-const another = new Cirlce(3);
+const another = new Circle(3);
 //1. New Creates Empty Object. 2. Sets object to point to Circle Object. 3. Passes Values.
 another.draw();
 
@@ -46,3 +46,19 @@ another.draw();
 // and new keyword means its a constructor.
 // C# Developers might be familiar with the new keyword, providing a new instance
 // of a class, but again in JavaScript we don't really have that concept, its sytacic sugar.
+
+//Lecture 5 - Contrsuctor Factory:
+//Every object in JavaScript has a constructor function, a way to display the method
+//that created the given object.
+
+console.log(Circle.constructor());
+
+let x = {};
+//Even Empty Object literals have the same constructor property.
+//same as let x = new Object();
+console.log(x.constructor);
+
+//We have a few other built in contructors.
+new String(); //same as '', "", ``
+new Boolean(); //same as true, false
+new Number(); //same as 1, 2, 3
