bfba3352d79e642dd88ba6d2c1294173cf4d1c28
Conor McGrath <conormcgrath123@gmail.com>
2021-06-20 19:01:59 +0100

    part 2 - prototypes - property descriptors

--- Part 2 - OOP/3 - Prototypes/starting-point/index.js
+++ Part 2 - OOP/3 - Prototypes/starting-point/index.js
@@ -1,1 +1,39 @@
-let myArray = [];
+//Lecture 4 - Property Descriptors:
+
+let person = { name: "Mosh" };
+console.log(person);
+//After logging person we can still all the Protype methods eg toString()
+
+//But if we look in the keys, we cant?
+for (let key in person) {
+  console.log(key);
+}
+
+//Same with Object Keys
+Object.keys(person);
+
+//Some times our properties have attributes, and these attributes prevent enumeration.
+let objectBase = Object.getPrototypeOf(person);
+let descriptor = Object.getOwnPropertyDescriptor(objectBase, "toString");
+
+console.log(descriptor);
+//Results
+//-Configureable true - means we can delete
+//-enumerable - why we cant iterate over this...
+//-writeable - means we can override it
+//value - the function used when we call toString()
+
+//Task - Getting our own Attributes:
+Object.defineProperty(person, "name", {
+  writable: false, //read only now
+  enumerable: false, //cant iterate now
+  configurable: false, //cant delete now
+});
+
+person.name = "john";
+console.log("name - ", person.name);
+console.log(Object.getPrototypeOf(person));
+delete person.name;
+//now we can no longer change the name, iterate over its properties or delete name.
+
+//Take home - use property descriptors to configure how our object properties are used.
