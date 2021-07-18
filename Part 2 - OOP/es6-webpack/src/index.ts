9a6716fc3bf24e70604511876c24a32ffc592c83
Conor McGrath <conormcgrath123@gmail.com>
2021-07-18 19:33:49 +0100

    part 2 - ES6 Tooling - webpack

---
+++ Part 2 - OOP/es6-webpack/src/index.ts
@@ -0,0 +1,8 @@
+import { Circle } from "./circle.js";
+
+const c = new Circle(10);
+console.log("Using Circle - ", c);
+//console.log("Attempting to Access Private members - ", c._radius);
+c.draw();
+
+console.log('Changes!')
